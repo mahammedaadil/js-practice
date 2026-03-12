8420d035c3cf575ec167e826ba4df1d04e70a2e8
mahammedaadil patel <adilchoice30@gmail.com>
2026-03-12 14:52:06 +0530

    merged 2 arrays program

---
+++ merge2Array.js
@@ -0,0 +1,10 @@
+let arr = [10, 20, 30, 3, 66, 22, 33, 22, 99, 2, 2, 4, 5, 7];
+let arr2 = [10, 20, 30, 3, 66, 22, 33, 22, 99, 2, 2, 4, 5, 7];
+
+// //Method 1
+// let arr3 = [...arr, ...arr2];
+// console.log(arr3);
+
+// //Method 2
+// let arr3 = arr.concat(arr2);
+// console.log(arr3);
